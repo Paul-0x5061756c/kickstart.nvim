75dfb632578989dd5873a020158c8d3dee655409
pabo <paul.den.boer@dataexcellence.nl>
2025-12-03 10:50:11 +0100

    added blink

---
+++ lua/custom/plugins/blink.lua
@@ -0,0 +1,12 @@
+return {
+  {
+    'saghen/blink.cmp',
+    version = '*',
+    opts = {
+      keymap = { preset = 'default' },
+      completion = {
+        menu = { auto_show = true },
+      },
+    },
+  },
+}
